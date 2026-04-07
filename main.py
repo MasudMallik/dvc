47d51a155c7ed8ff9343974dca8c9b3e816c0d6e
MasudMallik <masudmallik96@gmail.com>
2026-04-07 21:43:27 +0530

    first raw data

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import os
+
+data={
+    "name":["a","b","c"],
+    "age":[10,12,13],
+}
+df=pd.DataFrame(data)
+
+os.makedirs("data",exist_ok=True)
+path=os.path.join("data","sample.csv")
+df.to_csv(path)
